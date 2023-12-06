ad5da67ca795cfa29c644e64817d1f6c040813d1
Jefferson Ondze Mangha <34817366+JeffersonOndzeMangha@users.noreply.github.com>
2023-12-06 12:34:20 -0600

    built player card list to show the two lists
    includes methods to add and remove players from favorites list

--- src/features/players/PlayerCardList.tsx
+++ src/features/players/PlayerCardList.tsx
@@ -1,55 +1,70 @@
-import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Button, IconButton, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import MainCard from "../../app/components/MainCard";
+import { Player } from "../../app/types/Player";
+import { useAppSelector } from "../../app/hooks";
+import { StarOutline, StarTwoTone } from "@material-ui/icons";
 
 interface CardListProps {
-    list: any[];
+    list: Player[];
     title: string;
-    addToFavorites: (player: any) => void;
+    addToFavorites?: (player: any) => void;
     removeFromFavorites: (player: any) => void;
 }
 
 const PlayerCardList = (props: CardListProps) => {
     const { list, title, addToFavorites, removeFromFavorites } = props;
+    const { favoritePlayers } = useAppSelector((state) => state.players);
+
+    const manageFavorites = (player: any) => {
+        if (favoritePlayers[player.id]) {
+            removeFromFavorites(player);
+        } else {
+            if (addToFavorites) addToFavorites(player);
+        }
+    }
 
     return (
         <MainCard title={title}>
-            <TableContainer>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Name</TableCell>
-                            <TableCell>Position</TableCell>
-                            <TableCell>Team</TableCell>
-                            <TableCell>Actions</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {list.map((player: any) => (
-                            <TableRow key={player.id}>
-                                <TableCell>{player.name}</TableCell>
-                                <TableCell>{player.position}</TableCell>
-                                <TableCell>{player.team}</TableCell>
-                                <TableCell>
-                                    <Button
-                                        variant="contained"
-                                        color="primary"
-                                        onClick={() => addToFavorites(player)}
-                                    >
-                                        Add to favorites
-                                    </Button>
-                                    <Button
-                                        variant="contained"
-                                        color="secondary"
-                                        onClick={() => removeFromFavorites(player)}
-                                    >
-                                        Remove from favorites
-                                    </Button>
-                                </TableCell>
+            {
+                list.length == 0 &&
+                <Typography>No players in this list.</Typography>
+            }
+            {
+                list.length > 0 &&
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>Position</TableCell>
+                                <TableCell>Team</TableCell>
+                                <TableCell>Actions</TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                        </TableHead>
+                        <TableBody>
+                            {list.map((player: any) => (
+                                <TableRow key={player.id}>
+                                    <TableCell><Typography>{player.first_name} {player.last_name}</Typography></TableCell>
+                                    <TableCell><Typography>{player.position}</Typography></TableCell>
+                                    <TableCell><Typography>{player.team.full_name}</Typography></TableCell>
+                                    <TableCell>
+                                        <IconButton
+                                            onClick={() => manageFavorites(player)}
+                                            color={favoritePlayers[player.id] ? 'warning' : 'primary'}
+                                        >
+                                            {
+                                                favoritePlayers[player.id] ?
+                                                    <StarTwoTone /> :
+                                                    <StarOutline />
+                                            }
+                                        </IconButton>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            }
         </MainCard>
     );
 };
