40cf468e68adfe45c4ba4031847e3a632f008737
Jefferson Ondze Mangha <34817366+JeffersonOndzeMangha@users.noreply.github.com>
2023-12-06 12:34:48 -0600

    added fetch method to get data from api endpoint

--- src/features/players/playersAPI.ts
+++ src/features/players/playersAPI.ts
@@ -1,6 +1,17 @@
 // A mock function to mimic making an async request for data
-export function fetchPlayers(search?: string) {
-  return new Promise<{ data: any[] }>((resolve) =>
-    setTimeout(() => resolve({ data: [] }), 500)
+export function fetchPlayers(search?: string, filters?: {
+  [key: string]: string | number;
+}) {
+  return new Promise<{ data: {data: any[], meta: any} }>((resolve, reject) =>
+    fetch(`https://www.balldontlie.io/api/v1/players?${
+      search ? `search=${search}` : ''
+    }${
+      filters ? `&${Object.keys(filters).map((key) => `${key}=${filters[key]}`).join('&')}` : ''
+    }`).then((response) =>
+      response
+        .json()
+        .then((resp) => resolve(resp))
+        .catch((error) => reject(error))
+    )
   );
 }
