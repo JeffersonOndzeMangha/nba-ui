fd15331ac28b61ad45f681817ba2a0d069b71e87
Jefferson Ondze Mangha <34817366+JeffersonOndzeMangha@users.noreply.github.com>
2023-12-14 19:47:45 -0600

    updating return types

--- src/app/components/CircularProgressWithLabel.tsx
+++ src/app/components/CircularProgressWithLabel.tsx
@@ -11,13 +11,13 @@ import Box from '@mui/material/Box';
  * @param {object} props - Props for the CircularProgressWithLabel component.
  * @param {string} props.label - The label text to be displayed.
  * @param {CircularProgressProps} props - Additional props for the CircularProgress component.
- * @returns {React.Element} A React element that renders the circular progress indicator with a label.
+ * @returns {JSX.Element} A React element that renders the circular progress indicator with a label.
  *
  * @example
  * // Example usage of CircularProgressWithLabel:
  * <CircularProgressWithLabel label="Loading..." color="primary" size={50} />
  */
-function CircularProgressWithLabel(props: CircularProgressProps & { label: string }) {
+function CircularProgressWithLabel(props: CircularProgressProps & { label: string }): JSX.Element {
   return (
     <Box sx={{ position: 'relative' }}>
       <CircularProgress variant="indeterminate" {...props} />
